2e80de52e75b72fa8b243ab7771aef0abadaa3c4
Asrar Ali <mohdasrar3825@gmail.com>
2025-05-04 18:45:18 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Project--CV-Application/',
+  base: '/Project--CV-Application/', // Replace with your repository name
 });
